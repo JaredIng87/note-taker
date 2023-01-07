d273bc8442da36f1f23b5dbee2541babd2c68788
JaredIng87 <jaredingersoll14@gmail.com>
2023-01-07 13:32:22 -0500

    include Clog middleware

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
+const { clog } = require('./middleware/clog');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
 const noteData = require('./db/notes.json');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(clog);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
