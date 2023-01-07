19e07442893344a9123f7917f6ba07f1c69dc5c6
JaredIng87 <jaredingersoll14@gmail.com>
2023-01-07 14:03:24 -0500

    included fsutils custom middleware

--- index.js
+++ index.js
@@ -1,80 +1,31 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const fs = require('fs');
-const uuid = require('./helpers/uuid');
-const noteData = require('./db/notes.json');
+const api = require('./routes/index.js');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Import custom middleware, "cLog"
 app.use(clog);
 
-app.use(express.urlencoded({ extended: true }));
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'));
-});
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-app.get('/api/notes', (req, res) => res.json(noteData));
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
-
-    const { title, text } = req.body;
-
-    if (title&&text) {
-
-        const newNote = {
-            title,
-            text,
-            id: uuid(),
-            
-        };
-
-        fs.readFile('./db/notes.json', 'utf8', (err, data) => {
-            if (err) {
-              console.error(err);
-            } else {
-              // Convert string into JSON object
-              const parsedNotes = JSON.parse(data);
-      
-              // Add a new note
-              parsedNotes.push(newNote);
-      
-              // Write updated note back to the file
-              fs.writeFile(
-                './db/notes.json',
-                JSON.stringify(parsedNotes, null, 8),
-                (writeErr) =>
-                  writeErr
-                    ? console.error(writeErr)
-                    : console.info('Successfully updated notes!')
-              );
-            }
-          });
-    
-        const response = {
-          status: 'success',
-          body: newNote,
-        };
-    
-        console.log(response);
-        res.status(201).json(response);
-      } else {
-        res.status(500).json('Error in posting note');
-      }
-    });
-
-
-
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
